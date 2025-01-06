1370d5e192f8f93f0a13e6c241e988c4b2de0887
Vidhi <Vidhisharma6658@gmail.com>
2025-01-06 23:21:06 +0530

    EDA AND MODEL TRAINING

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 import logging
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -18,10 +19,10 @@ class CustomException(Exception):
          return self.error_message
     
 
-# if __name__== "__main__":
-#     try:
-#          a=1/0
-#     except Exception as e:
-#         logging.info("devide by 0 error")
-#         raise CustomException(e, sys)
+if __name__== "__main__":
+    try:
+         a=1/0
+    except Exception as e:
+        logging.info("devide by 0 error")
+        raise CustomException(e, sys)
    
