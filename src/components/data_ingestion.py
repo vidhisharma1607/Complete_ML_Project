ca3d112a38e1021f513a05fbb91a248e601b79e6
Vidhi <Vidhisharma6658@gmail.com>
2025-01-07 13:41:46 +0530

    model training done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,8 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import MOdelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 print("Current sys.path:", sys.path)
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.exception import CustomException
@@ -51,5 +52,7 @@ if __name__=="__main__":
     train_data, test_data= obj.initiate_data_ingestion()
 
     data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
 
+    modelTrainer= MOdelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr, test_arr))
