06034200231902a25912754234d82aac3dd1d767
Vidhi <Vidhisharma6658@gmail.com>
2025-01-07 19:03:19 +0530

    web app using flask

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 def save_object(file_path, obj):
@@ -33,3 +34,11 @@ def evaluate_models(x_train, y_train, x_test, y_test,models):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
